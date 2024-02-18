babc61ac750934502bfd10c7a596c8ed559a54c1
Mattew Razinov <chikernut213@gmail.com>
2024-02-18 17:41:59 +0400

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	new file:   PR18_8/.vs/PR18_8/DesignTimeBuild/.dtbcache.v2
    	new file:   PR18_8/.vs/PR18_8/FileContentIndex/14d4ddf9-767f-4797-b719-c5f4da6a77ac.vsidx
    	new file:   PR18_8/.vs/PR18_8/FileContentIndex/1b0019a6-38c1-4036-8266-ceeed6518910.vsidx
    	new file:   PR18_8/.vs/PR18_8/FileContentIndex/50ea4223-848f-4f97-954e-6874f8e9df1d.vsidx
    	new file:   PR18_8/.vs/PR18_8/FileContentIndex/9d6b6e8e-310a-45ff-b397-c38bca1e87a2.vsidx
    	new file:   PR18_8/.vs/PR18_8/FileContentIndex/read.lock
    	new file:   PR18_8/.vs/PR18_8/v17/.futdcache.v2
    	new file:   PR18_8/.vs/PR18_8/v17/.suo
    	new file:   PR18_8/.vs/ProjectEvaluation/pr18_8.metadata.v5.2
    	new file:   PR18_8/.vs/ProjectEvaluation/pr18_8.projects.v5.2
    	new file:   PR18_8/PR18_8.sln
    	new file:   PR18_8/PR18_8/.vs/PR18_8/DesignTimeBuild/.dtbcache.v2
    	new file:   PR18_8/PR18_8/.vs/PR18_8/FileContentIndex/5698fd33-22e3-4f40-b733-0df6bdfd527c.vsidx
    	new file:   PR18_8/PR18_8/.vs/PR18_8/FileContentIndex/cd632fb0-60e4-4c35-bae6-8d774b7b9f73.vsidx
    	new file:   PR18_8/PR18_8/.vs/PR18_8/FileContentIndex/read.lock
    	new file:   PR18_8/PR18_8/.vs/PR18_8/v17/.suo
    	new file:   PR18_8/PR18_8/Friend.cs
    	new file:   PR18_8/PR18_8/Ogranization.cs
    	new file:   PR18_8/PR18_8/PR18_8.csproj
    	new file:   PR18_8/PR18_8/Person.cs
    	new file:   PR18_8/PR18_8/Program.cs
    	new file:   PR18_8/PR18_8/TelephoneGuide.cs
    	new file:   PR18_8/PR18_8/bin/Debug/net6.0/PR18_8.deps.json
    	new file:   PR18_8/PR18_8/bin/Debug/net6.0/PR18_8.dll
    	new file:   PR18_8/PR18_8/bin/Debug/net6.0/PR18_8.exe
    	new file:   PR18_8/PR18_8/bin/Debug/net6.0/PR18_8.pdb
    	new file:   PR18_8/PR18_8/bin/Debug/net6.0/PR18_8.runtimeconfig.json
    	new file:   PR18_8/PR18_8/data.txt
    	new file:   PR18_8/PR18_8/find_res.txt
    	new file:   PR18_8/PR18_8/input.dat
    	new file:   PR18_8/PR18_8/obj/Debug/net6.0/.NETCoreApp,Version=v6.0.AssemblyAttributes.cs
    	new file:   PR18_8/PR18_8/obj/Debug/net6.0/PR18_8.AssemblyInfo.cs
    	new file:   PR18_8/PR18_8/obj/Debug/net6.0/PR18_8.AssemblyInfoInputs.cache
    	new file:   PR18_8/PR18_8/obj/Debug/net6.0/PR18_8.GeneratedMSBuildEditorConfig.editorconfig
    	new file:   PR18_8/PR18_8/obj/Debug/net6.0/PR18_8.GlobalUsings.g.cs
    	new file:   PR18_8/PR18_8/obj/Debug/net6.0/PR18_8.assets.cache
    	new file:   PR18_8/PR18_8/obj/Debug/net6.0/PR18_8.csproj.AssemblyReference.cache
    	new file:   PR18_8/PR18_8/obj/Debug/net6.0/PR18_8.csproj.BuildWithSkipAnalyzers
    	new file:   PR18_8/PR18_8/obj/Debug/net6.0/PR18_8.csproj.CoreCompileInputs.cache
    	new file:   PR18_8/PR18_8/obj/Debug/net6.0/PR18_8.csproj.FileListAbsolute.txt
    	new file:   PR18_8/PR18_8/obj/Debug/net6.0/PR18_8.dll
    	new file:   PR18_8/PR18_8/obj/Debug/net6.0/PR18_8.genruntimeconfig.cache
    	new file:   PR18_8/PR18_8/obj/Debug/net6.0/PR18_8.pdb
    	new file:   PR18_8/PR18_8/obj/Debug/net6.0/apphost.exe
    	new file:   PR18_8/PR18_8/obj/Debug/net6.0/ref/PR18_8.dll
    	new file:   PR18_8/PR18_8/obj/Debug/net6.0/refint/PR18_8.dll
    	new file:   PR18_8/PR18_8/obj/PR18_8.csproj.nuget.dgspec.json
    	new file:   PR18_8/PR18_8/obj/PR18_8.csproj.nuget.g.props
    	new file:   PR18_8/PR18_8/obj/PR18_8.csproj.nuget.g.targets
    	new file:   PR18_8/PR18_8/obj/project.assets.json
    	new file:   PR18_8/PR18_8/obj/project.nuget.cache
    	new file:   PR18_8/PR18_8/sort_res.txt
    	new file:   PR18_8/PR18_8/table.txt

---
+++ PR18_8/PR18_8/Ogranization.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telephone;
+
+namespace Telephone
+{
+    [Serializable]
+    public class Ogranization : Person
+    {
+        protected string orgname;
+        protected string faks;
+        readonly string type_k = "Org";
+        // конструктор
+        public Ogranization(string surname, string addres, string number, string faks, string orgname)
+        : base(surname, addres, number)
+        {
+            this.faks = faks;
+            this.orgname = orgname;
+        }
+        public override void GetInfo()
+        {
+            Console.Write($"Контакт лицо: {surname}");
+            Console.Write($" Адрес: {addres}");
+            Console.Write($" тел.номер: {number}");
+            Console.Write($" факс: {faks}");
+            Console.Write($" название: {orgname}\n");
+        }
+        public override void Set(params string[] args)
+        {
+            if (args.Length == 5)
+            {
+                this.surname = args[0];
+                this.addres = args[1];
+                this.number = args[2];
+                this.faks = args[3];
+                this.orgname = args[4];
+            }
+            else
+            {
+
+                throw new Exception($"Ошибка в кол-ве аргументов, принято: {args.Length}, необходимо: 5");
+            }
+        }
+        public override bool[] InGuide(params string[] args)
+        {
+            string[] temp = { this.surname, this.addres, this.number, this.faks, this.orgname };
+            bool[] res = { false, false, false, false, false };
+            for (int i = 0; i < args.Length; i++)
+            {
+                res[i] = (temp[i] == args[i]);
+            }
+            return res;
+        }
+
+    }
+}
---
+++ PR18_8/PR18_8/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Collections.Generic;
+using Telephone;
+using System.Xml.Linq;
+
+namespace Telephone
+{
+    class Program
+    { 
+    static void Print(List<TelephoneGuide> TG)
+        {
+            Console.WriteLine("--------");
+            foreach (var t in TG)
+            {
+                t.GetInfo();
+            }
+            Console.WriteLine("--------");
+        }
+    static void Main(string[] args)
+    {
+        List<TelephoneGuide> TG = new List<TelephoneGuide>();
+        BinaryFormatter formatter = new BinaryFormatter();
+        using (FileStream f = new FileStream("C:\\Users\\chike\\source\\repos\\PR18_8\\PR18_8\\input.dat",
+         FileMode.OpenOrCreate))
+            {
+                if (f.Length != 0)
+                {
+                    TG = (List<TelephoneGuide>)formatter.Deserialize(f);
+                }
+            }
+            Print(TG);
+                    ////
+                    TG.Add(new Person("Ivanov", "pushkina dom kalatushkina", "99999"));
+                    TG.Add(new Person("Ivanov2", "pushkina1 dom kalatushkina", "99994"));
+                    TG.Add(new Person("Ivanov3", "pushkina2 dom kalatushkina", "99995"));
+                    TG.Add(new Person("Ivanov", "pushkina3 dom kalatushkina", "99996"));
+                    TG.Add(new Friend("Ivanov", "pushkina dom kalatushkina", "99999", "28.01.2004"));
+                    TG.Add(new Friend("Ivanov2", "pushkina1 dom kalatushkina", "99994", "15.03.1980"));
+                    TG.Add(new Ogranization("Ivanov", "pushkina2 dom kalatushkina", "99995", "12324", "345435"));
+                    TG.Add(new Ogranization("Ivanov4", "pushkina3 dom kalatushkina", "99996", "54354", "435435345"));
+                    ////
+            Print(TG);
+            //TG.Sort();
+            //Print(TG);
+            string[] sur = { "Ivanov" };
+            var findBySurname =
+                from pers in TG
+                where pers.InGuide(sur)[0] == true
+                select pers;
+            List<TelephoneGuide> find_sur = new List<TelephoneGuide>();
+            foreach(var s in findBySurname) {
+                find_sur.Add(s);
+            }
+            Print(find_sur);
+            using (FileStream f = new FileStream("C:\\Users\\chike\\source\\repos\\PR18_8\\PR18_8\\input.dat",
+                    FileMode.OpenOrCreate))
+                    {
+                        formatter.Serialize(f, TG);
+                    }           
+        }
+    }
+}
+
